236963f9e091e6b3ab8b19a6ca09c6140ddd6f2e
dachepally <46684673+dachepally@users.noreply.github.com>
2019-09-17 23:16:26 -0500

    Add files via upload
    Project added

---
+++ Dachepalli_Lab02Objects/src/StringsAndNumbers.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sai Sri Dachepalli
+ */
+import java.io.*;
+import java.util.*;
+
+public class StringsAndNumbers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        private int isbn;
+        private String bookName;
+        //    public StringsAndNumbers(int isbn, String bookName)
+        //    {            
+        //         this.isbn = isbn;
+        //         this.bookName = bookName;
+        //    }
+
+        //  StringsAndNumbers book1 = new StringsAndNumbers(1234,"OOPS");
+
+
+
+    
+    public void setBookName(String bookNameIn) {
+        bookName = bookNameIn;
+    }
+
+    public String toString() {
+        return isbn + " " + bookName;
+    }
+
+    System.out.println ()
+
+    System.out.println(book1);
+    bogi1.setBookName (“HCI”);
+
+    /**
+     * System.out.println (book1.toString
+
+());
+}
+
+        
+    }
+    
+}
---
+++ Dachepalli_Lab02Objects/src/Test.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sai Sri Dachepalli
+ */
+import java.util.*;
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//Scanner sc = new Scanner(System.in);
+//int num = sc.nextInt();
+////sc.nextLine();
+//int num2 = sc.nextInt();
+//        System.out.println(""+num + num2);
+//        
+//  
+//int[] playersWeight = new int []{1,2,3,4,5};
+//for(int d : playersWeight){
+//System.out.print(d);
+    int isbn = 0;
+    String bookName = "hello";
+    
+    public Test(int isbnIn, String bookNameIn){            
+         this.isbn = isbnIn;
+         this.bookName = bookNameIn;
+    
+   Test book1 = new Test(1234,"OOPS");
+
+     
+
+   
+  }
+
+
+System.out.println(book1);
+//bogi1.setBookName(“HCI”);
+System.out.println(book1.toString());
+    }
+}//end BookTester class
+
+
+}
+
+        
+        
+// TODO code application logic here
+    }
+    
+}
